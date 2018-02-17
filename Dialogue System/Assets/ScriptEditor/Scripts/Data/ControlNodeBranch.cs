e8e4c424075f0e7b2b8635f505dc086c92b98b39
Siddharth Ghatti <sg4ff@virginia.edu>
2018-02-17 14:10:12 -0500

    The Other half ot the conversions

---
+++ Dialogue System/Assets/ScriptEditor/Scripts/Data/ControlNodeBranch.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+public class ControlNodeBranch
+{
+    public ControlNodeBranch()
+    {
+
+
+    }
+}
